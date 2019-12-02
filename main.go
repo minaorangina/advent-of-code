d718e08032b1f8172e210632dcdcfa40ed47c4e9
minaorangina <mina@pusher.com>
2019-12-02 17:56:00 +0000

    adds 2nd day

--- main.go
+++ main.go
@@ -16,24 +16,31 @@ func check(e error) {
 }
 
 func main() {
-	day1()
+	file, err := os.Open("input/01.txt")
+	check(err)
+	defer file.Close()
+
+	day1(file)
+	day2(file)
 }
 
 func getMass(number int) int {
 	return number/3 - 2
 }
+func getMassRecursive(number int) int {
+	fuel := number/3 - 2
+	if fuel <= 0 {
+		return 0
+	}
+	return fuel + getMassRecursive(fuel)
+}
 
-func day1() {
-	file, err := os.Open("input/01.txt")
-	check(err)
-	defer file.Close()
-
+func day1(file *os.File) {
 	reader := bufio.NewReader(file)
 	var sum int
-	var line string
 
 	for {
-		line, err = reader.ReadString('\n')
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
@@ -45,4 +52,28 @@ func day1() {
 		sum += getMass(num)
 	}
 	fmt.Println("Day 01:", sum)
+}
+
+func day2(file *os.File) {
+	// keep getting zero if this isn't here...
+	file, err := os.Open("input/01.txt")
+	check(err)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var sum int
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		trimmedLine := strings.TrimSuffix(line, "\n")
+		num, err := strconv.Atoi(trimmedLine)
+		check(err)
+
+		sum += getMassRecursive(num)
+	}
+	fmt.Println("Day 02:", sum)
 }
