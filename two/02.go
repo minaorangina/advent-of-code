0567ce775295bd0b88cf7d80f31190dc3b43b4bf
minaorangina <mina@pusher.com>
2019-12-03 19:13:19 +0000

    adds day 2, part 2

--- two/02.go
+++ two/02.go
@@ -11,38 +11,43 @@ import (
 
 func Exec() {
 	fmt.Println("Day 2")
-	part1()
+	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
 }
 
-func part1() {
-	type Op int
-
-	const (
-		Add      Op = 1
-		Multiply Op = 2
-		Exit     Op = 99
-	)
+func part1() int {
 
 	values := getValues()
 
-	values[1] = 12
-	values[2] = 2
+	// inputs
+	values[1] = 12 // noun
+	values[2] = 2  // verb
 
-	for i := 0; i < len(values); i += 4 {
-		switch Op(values[i]) {
-		case Add:
-			values[values[i+3]] = values[values[i+1]] + values[values[i+2]]
-		case Multiply:
-			values[values[i+3]] = values[values[i+1]] * values[values[i+2]]
-		case Exit:
-			break
-		default:
-			// fmt.Println("Unknown op code")
-			break
+	return runIntCode(values)
+}
+
+func part2() int {
+	const max = 99
+	const expected = 19690720
+
+	values := getValues()
+
+	for noun := 1; noun < 100; noun++ {
+		for verb := 1; verb < 100; verb++ {
+
+			copiedValues := make([]int, len(values))
+			copy(copiedValues, values)
+			copiedValues[1] = noun
+			copiedValues[2] = verb
+
+			result := runIntCode(copiedValues)
+
+			if result == expected {
+				return 100*noun + verb
+			}
 		}
 	}
-
-	fmt.Println("	Part 1:", values[0])
+	panic("Failed")
 }
 
 func getValues() []int {
@@ -63,4 +68,30 @@ func getValues() []int {
 		values = append(values, num)
 	}
 	return values
+}
+
+func runIntCode(values []int) int {
+	type Op int
+
+	const (
+		Add      Op = 1
+		Multiply Op = 2
+		Exit     Op = 99
+	)
+
+	for i := 0; i < len(values); i += 4 {
+		switch Op(values[i]) {
+		case Add:
+			values[values[i+3]] = values[values[i+1]] + values[values[i+2]]
+		case Multiply:
+			values[values[i+3]] = values[values[i+1]] * values[values[i+2]]
+		case Exit:
+			break
+		default:
+			// fmt.Println("Unknown op code")
+			break
+		}
+	}
+
+	return values[0]
 }
