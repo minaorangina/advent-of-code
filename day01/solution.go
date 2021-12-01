0704e010e622c183434f58ad40c82916c2e144b1
Mina <wilhelmina.jane@gmail.com>
2021-12-01 19:38:32 +0000

    day 1, part 2

--- day01/solution.go
+++ day01/solution.go
@@ -1,5 +1,7 @@
 package day01
 
+import "fmt"
+
 func Part1(input []int) int {
 	var count int
 
@@ -16,5 +18,25 @@ func Part1(input []int) int {
 }
 
 func Part2(input []int) int {
-	return 0
+	var count int
+
+	for i := range input {
+		if i == 0 {
+			continue
+		}
+		if i+2 >= len(input) {
+			break
+		}
+
+		var sumWindow, sumPrevWindow int
+		sumWindow += input[i] + input[i+1] + input[i+2]
+		sumPrevWindow += input[i-1] + input[i] + input[i+1]
+
+		if sumWindow > sumPrevWindow {
+			fmt.Println("increased")
+			count++
+		}
+	}
+
+	return count
 }
--- day01/solution_test.go
+++ day01/solution_test.go
@@ -8,19 +8,17 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	t.Run("testInput", func(t *testing.T) {
-		testInput := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	testInput := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 
-		got := Part1(testInput)
+	got := Part1(testInput)
 
-		if got != 7 {
-			t.Error()
-		}
-	})
+	if got != 7 {
+		t.Error()
+	}
 
 	t.Run("real thing", func(t *testing.T) {
-		testInput := data.ToIntSlice(data.Parse())
-		fmt.Println(Part1(testInput))
+		input := data.ToIntSlice(data.Parse())
+		fmt.Println(Part1(input))
 	})
 }
 
@@ -31,4 +29,9 @@ func TestPart2(t *testing.T) {
 	if got != 5 {
 		t.Error()
 	}
+
+	t.Run("real thing", func(t *testing.T) {
+		input := data.ToIntSlice(data.Parse())
+		fmt.Println(Part2(input))
+	})
 }
