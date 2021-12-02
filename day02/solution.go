0efa42809f50d6b41fcbdf3b9602c8bd4bc62257
Mina <wilhelmina.jane@gmail.com>
2021-12-02 11:57:51 +0000

    day 2, part 2

--- day02/solution.go
+++ day02/solution.go
@@ -42,6 +42,36 @@ func Part1(input []byte) (int, error) {
 	return submarine.multiply(), nil
 }
 
+func Part2(input []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+	scanner.Split(bufio.ScanLines)
+
+	submarine := coords{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		parts := strings.Fields(string(line))
+
+		if len(parts) != 2 {
+			return 0, fmt.Errorf("found invalid line: %v", parts)
+		}
+		if !validCommand(parts[0]) {
+			return 0, fmt.Errorf("bad input: %s", parts[0])
+		}
+
+		var num int
+		num, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return 0, err
+		}
+
+		submarine.moveWithAim(parts[0], num)
+	}
+
+	return submarine.multiply(), nil
+}
+
 func validCommand(s string) bool {
 	switch s {
 	case "forward", "down", "up":
@@ -52,7 +82,7 @@ func validCommand(s string) bool {
 }
 
 type coords struct {
-	x, y int
+	x, y, z int
 }
 
 func (c *coords) move(command string, distance int) {
@@ -63,6 +93,20 @@ func (c *coords) move(command string, distance int) {
 		c.y += distance
 	case "up":
 		c.y -= distance
+	default:
+		panic(fmt.Sprintf("bad input: %s", command))
+	}
+}
+
+func (c *coords) moveWithAim(command string, distance int) {
+	switch command {
+	case "forward":
+		c.x += distance
+		c.y += c.z * distance
+	case "down":
+		c.z += distance
+	case "up":
+		c.z -= distance
 	default:
 		panic(fmt.Sprintf("bad input: %s", command))
 	}
--- day02/solution_test.go
+++ day02/solution_test.go
@@ -32,3 +32,30 @@ func TestPart1(t *testing.T) {
 		fmt.Printf("Day 2, part 1: %d", res)
 	})
 }
+func TestPart2(t *testing.T) {
+	t.Run("test case", func(t *testing.T) {
+		testInput := []byte(`forward 5
+		down 5
+		forward 8
+		up 3
+		down 8
+		forward 2`)
+
+		res, err := Part2(testInput)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if res != 900 {
+			t.Fatalf("wanted 900, got %d", res)
+		}
+	})
+
+	t.Run("real thing", func(t *testing.T) {
+		res, err := Part2(input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Printf("Day 2, part 2: %d", res)
+	})
+}
