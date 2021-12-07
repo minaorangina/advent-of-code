6df641a2bda7407dc3ed8a2100c4e030ffd12284
Mina <wilhelmina.jane@gmail.com>
2021-12-07 18:52:03 +0000

    day 6, part 2

--- day06/solution.go
+++ day06/solution.go
@@ -1,66 +1,31 @@
 package day06
 
-import (
-	"log"
-	"strconv"
-	"strings"
-)
-
-func Part1(inputB []byte, days int) int {
-	asString := strings.Split((string(inputB)), ",")
-
-	var input []int
-	for _, c := range asString {
-		n, err := strconv.Atoi(c)
-		if err != nil {
-			log.Fatal(err)
-		}
-		input = append(input, n)
+func Solution(input []int, days int) int {
+	states := make([]int, 9)
+	for _, v := range input {
+		states[v]++
 	}
 
 	for d := 0; d < days; d++ {
-		for i := range input {
-			if input[i] == 0 {
-				input[i] = 6
-				input = append(input, 8)
+		newStates := make([]int, 9)
+		for i, v := range states {
+			if v == 0 {
 				continue
 			}
-			input[i]--
+			if i == 0 {
+				newStates[6] = v
+				newStates[8] = v
+				continue
+			}
+			newStates[i-1] += v
 		}
-	}
-	return len(input)
-}
-
-func Part2(inputB []byte) int {
-	asString := strings.Split((string(inputB)), ",")
-
-	var input []int
-	for _, c := range asString {
-		n, err := strconv.Atoi(c)
-		if err != nil {
-			log.Fatal(err)
-		}
-		input = append(input, n)
+		states = newStates
 	}
 
-	var results []int
-	for _, v := range input {
-		results = append(results, doStuff(v)...)
+	var count int
+	for _, v := range states {
+		count += v
 	}
-	return len(results)
+
+	return count
 }
-
-func doStuff(n int) []int {
-	return nil
-}
-
-/*
-Rules
-- each fish <7 has the same number after 7 days
-- each new fish will have 1 day less than its parent
-
-- each fish  >=7 has self-7 after 7 days
-
-q - give me a new number
-q - give me a new fish (if i should have one	)
-*/
--- day06/solution_test.go
+++ day06/solution_test.go
@@ -2,7 +2,10 @@ package day06
 
 import (
 	_ "embed"
+	"fmt"
 	"testing"
+
+	"github.com/minaorangina/advent-of-code-2021/parse"
 )
 
 var (
@@ -12,7 +15,7 @@ var (
 	input []byte
 )
 
-func TestPart1(t *testing.T) {
+func TestSolution(t *testing.T) {
 	tt := []struct {
 		days, want int
 	}{
@@ -30,13 +33,14 @@ func TestPart1(t *testing.T) {
 		}}
 
 	for _, tc := range tt {
-		got := Part1(testInput, tc.days)
+		got := Solution(parse.ToIntSlice(testInput), tc.days)
 		if got != tc.want {
 			t.Errorf("got %d, want %d", got, tc.want)
 		}
 	}
 
-	// t.Run("real thing", func(t *testing.T) {
-	// 	fmt.Println(Part1(input, 80))
-	// })
+	t.Run("real thing", func(t *testing.T) {
+		fmt.Println(Solution(parse.ToIntSlice(input), 80))
+		fmt.Println(Solution(parse.ToIntSlice(input), 256))
+	})
 }
