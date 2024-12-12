bbf69f01b2ce1a50edace3a5cad9b6d638d67462
Mina <wilhelmina.jane@gmail.com>
2024-12-12 17:46:14 +0000

    day 12 part 1?

--- 2024/day12.py
+++ 2024/day12.py
@@ -16,16 +16,16 @@ class Coords:
         return self.r * len(self.grid[0]) + self.c
 
     def __repr__(self):
-        if self == UP:
-            return "UP"
-        elif self == DOWN:
-            return "DOWN"
-        elif self == LEFT:
-            return "LEFT"
-        elif self == RIGHT:
-            return "RIGHT"
-        else:
-            return f"({self.get_char()}@{self.r},{self.c})"
+        # if self == UP:
+        #     return "UP"
+        # elif self == DOWN:
+        #     return "DOWN"
+        # elif self == LEFT:
+        #     return "LEFT"
+        # elif self == RIGHT:
+        #     return "RIGHT"
+        # else:
+        return f"({self.get_char()}@{self.r},{self.c})"
         
     def get_char(self):
         if not self.is_valid():
@@ -41,6 +41,10 @@ class Coords:
     def get_valid_neighbours(self):
         return [i for i in self.get_neighbours() if i.is_valid()]
 
+    def get_region_neighbours(self):
+        valid = self.get_valid_neighbours()
+        return [n for n in valid if n.get_char() == self.get_char()]
+
     
 UP = Coords(-1, 0)
 DOWN = Coords(1, 0)
@@ -54,28 +58,26 @@ def parse(path):
         input = [x.strip() for x in input]
         return input
 
-
-
-    # return locations
-
-def do(curr, visited, regions, key):
+def do(curr,key, visited=[], regions={},perimeters={}):
     if curr in visited:
         return
     
     regions[key].append(curr)
     visited.add(curr)
-    
-    all_neighbours = curr.get_valid_neighbours()
-    # print("all neighbours", all_neighbours)
 
-    for n in all_neighbours:
-        if n.get_char() == curr.get_char():
-            do(n, visited, regions, key)
+    region_neighbours = curr.get_region_neighbours()
+    perimeter = 4 - len(region_neighbours)
+    perimeters[key] += perimeter
+    
+
+    for n in region_neighbours:
+        do(n, key, visited=visited, regions=regions, perimeters=perimeters)
 
 
 
 def part1(input):
     regions = defaultdict(list)
+    perimeters = defaultdict(int)
 
     visited = set()
     
@@ -86,14 +88,18 @@ def part1(input):
             # print(f"now looking at {char} at loc({ri},{ci})")
 
             curr = Coords(ri, ci, input)
-            do(curr, visited, regions, str(curr))
+            do(curr,str(curr), visited, regions, perimeters)
 
     print(regions)
+    print(perimeters)
+
+    count = 0
+    for k in regions.keys():
+        count += len(regions[k]) * perimeters[k]
+    print(count)
 
 
-
-
-input = parse("input/day12_example.txt")
+input = parse("input/day12.txt")
 n_rows = len(input)
 n_cols = len(input[0])
 
