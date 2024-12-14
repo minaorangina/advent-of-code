8cff8d660c241818b4704d11714e2d075e5cd295
Mina <wilhelmina.jane@gmail.com>
2024-12-14 22:02:54 +0000

    fix

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,8 +1,8 @@
 import re
-max_row = 7
-max_col = 11
-# max_row = 103
-# max_col = 101
+# max_row = 7
+# max_col = 11
+max_row = 103
+max_col = 101
 
 class Coords:
     def __init__(self, r, c):
@@ -23,15 +23,6 @@ class Coords:
         return self.r == value.r and self.c == value.c
 
     def __repr__(self):
-        # if self == UP:
-        #     return "UP"
-        # elif self == DOWN:
-        #     return "DOWN"
-        # elif self == LEFT:
-        #     return "LEFT"
-        # elif self == RIGHT:
-        #     return "RIGHT"
-        # else:
         return f"({self.r},{self.c})"
         
     
@@ -42,13 +33,16 @@ UP = Coords(-1, 0)
 DOWN = Coords(1, 0)
 LEFT = Coords(0, -1)
 RIGHT = Coords(0, 1)
-
-with open("input/day14_example.txt", "r") as reader:
+# max_row = 7
+# max_col = 11
+# for i in range(6):
+#     print(Coords(4,2) + Coords(-3,2) * i)
+with open("input/day14.txt", "r") as reader:
     lines = reader.readlines()
 
     row_boundary = (max_row-1)/2
     col_boundary = (max_col-1)/2
-
+    
     upper_l, upper_r, bottom_l, bottom_r = 0,0,0,0
     for line in lines:
         # get position
@@ -56,9 +50,9 @@ with open("input/day14_example.txt", "r") as reader:
 
         # get velocity
         vel = [int(x) for x in (re.findall(r"(?<=v=)-?\d+,-?\d+", line)[0].split(","))]
-
-        robot = Coords(*position)
-        velocity = Coords(*vel)
+        
+        robot = Coords(position[1], position[0])
+        velocity = Coords(vel[1], vel[0])
 
         end = robot + (velocity * 100)
         
@@ -70,5 +64,6 @@ with open("input/day14_example.txt", "r") as reader:
             bottom_l += 1
         elif end.r > row_boundary and end.c > col_boundary:
             bottom_r += 1
-
+    
+    print(upper_l, upper_r, bottom_l, bottom_r)
     print(upper_l * upper_r * bottom_l * bottom_r)
